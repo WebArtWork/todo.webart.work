c84b9e478bc68552ae7952782158a7b36dcae651
Honchar Denys <crackeraki@gmail.com>
2026-08-31 01:39:11 +0300

    Add shared task-card component for boards, backlog, and sprint views

---
+++ src/app/shared/task-card/task-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, computed, inject, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TagModule } from '@wawjs/ngx-prime/tag';
+import { TranslateDirective } from '@wawjs/ngx-translate';
+import { TagsService } from '../../features/tags/tags.service';
+import { Task } from '../../features/tasks/task.interface';
+
+const PRIORITY_SEVERITY: Record<Task['priority'], 'danger' | 'warn' | 'secondary'> = {
+	high: 'danger',
+	medium: 'warn',
+	low: 'secondary',
+};
+
+@Component({
+	selector: 'app-task-card',
+	templateUrl: './task-card.component.html',
+	styleUrl: './task-card.component.scss',
+	imports: [RouterLink, TagModule, TranslateDirective],
+})
+export class TaskCardComponent {
+	private readonly _tagsService = inject(TagsService);
+
+	readonly task = input.required<Task>();
+
+	readonly tags = computed(() => this._tagsService.many(this.task().tagIds));
+	readonly prioritySeverity = computed(() => PRIORITY_SEVERITY[this.task().priority]);
+}
